d46b19567bb2d32eb02029722b7b284aed6698cd
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-06-22 12:35:38 +0530

    all files added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { AllRoutes } from './Pages/AllRoutes';
+import { Navbar } from './Pages/Navbar';
 
 function App() {
   return (
     <div className="App">
-   
+      <Navbar/>
+      <AllRoutes/>
     </div>
   );
 }
