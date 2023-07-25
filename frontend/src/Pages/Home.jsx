58bfdd99f0ec8b37a60b41935f0f67dbec6d4f01
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-07-25 11:23:39 +0530

    navbar is completed

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='grandFather'>
+      <Navbar/>
+
+    </div>
   )
 }
